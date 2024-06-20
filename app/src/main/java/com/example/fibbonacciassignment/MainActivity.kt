0377fa959f3489fcaa9e75b9d4b7dac2bb2a7c38
Loice-Staice <staiceloicea@gmail.com>
2024-06-20 13:23:01 +0300

    added files

---
+++ app/src/main/java/com/example/fibbonacciassignment/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fibbonacciassignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fibbonacciassignment.databinding.ActivityMainBinding
+import com.example.fibbonacciassignment.databinding.FibbonacciNumberViewBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val numbers = fibonacci(100)
+        binding.rvNumbers.layoutManager = LinearLayoutManager(this)
+
+        val numberAdapter = FibonacciRecyclerViewAdapter(numbers)
+        binding.rvNumbers.adapter = numberAdapter
+
+
+    }
+
+    fun fibonacci(n: Int): List<Int> {
+        val numbers = mutableListOf(0, 1)
+        while (numbers.size < n) {
+            numbers.add(numbers[numbers.lastIndex] + numbers[numbers.lastIndex - 1])
+        }
+        return numbers
+    }
+}
